83e551201b0dc4232e9d3139e4b8c3942c495bd6
Marco Jasso <68798766+DoesMK@users.noreply.github.com>
2021-03-09 14:58:42 -0600

    update htaccess

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include 'menu.php';
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge ">
     <link rel="stylesheet" href="css/styles.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Universidad de León</title>
