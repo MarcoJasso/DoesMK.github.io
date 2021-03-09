976217e967385188049f297adfcd5fa018fef387
Marco Jasso <68798766+DoesMK@users.noreply.github.com>
2021-03-09 13:35:41 -0600

    init commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include 'menu.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Universidad de León</title>
+</head>
+<body>
+
+</body>
+</html>
